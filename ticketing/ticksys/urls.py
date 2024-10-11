fa08271100b6d4d2b02414699bdba2e8bdd9a5a2
Magma4 <youngmagma@protonmail.com>
2024-10-11 13:04:21 +0000

    pages linked

--- ticketing/ticksys/urls.py
+++ ticketing/ticksys/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('index/', views.index, name='index'),
+    path('ticketlogs/', views.ticketlogs, name='ticketlogs'),
+    path('faqs/', views.faqs, name='faqs')
 ]
--- ticketing/ticksys/views.py
+++ ticketing/ticksys/views.py
@@ -4,3 +4,9 @@ from .models import *
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def ticketlogs(request):
+    return render(request, 'ticketlogs.html')
+
+def faqs(request):
+    return render(request, 'faqs.html')
